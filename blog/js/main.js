ee2b64dcec609827cd8e8ee5636ec8b9e6d80e85
mototakan <mototakan.1114@gmail.com>
2023-02-17 14:28:56 +0900

    first-commit

---
+++ blog/js/main.js
@@ -0,0 +1,37 @@
+// drawer-menu
+const target = document.getElementById("menu");
+target.addEventListener("click", () => {
+  const target = document.getElementById("menu");
+  target.classList.toggle("open");
+  const nav = document.getElementById("drawer-nav");
+  nav.classList.toggle("in");
+});
+
+// scrollTop
+function scrollTop(elem) {
+  const target = document.getElementById(elem);
+  target.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
+
+scrollTop("button");
+
+// modal
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn");
+const modal = document.getElementById("modal");
+openBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+window.addEventListener("click", (e) => {
+  if (!e.target.closest(".modal__content-inner") && e.target.id !== "openBtn") {
+    modal.style.display = "none";
+  }
+});
